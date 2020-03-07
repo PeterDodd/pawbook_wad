661199bdff2342d91a139ffda204bdc593f8e6be
Jake Haakanson <2407682H@student.gla.ac.uk>
2020-03-07 18:47:43 +0000

    Added forms for each model, date field for necessary models.

--- pawbook_wad/forms.py
+++ pawbook_wad/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
+from pawbook_wad.models import UserProfile, PetPedia, Post, Listing
+
 
 class UserForm(forms.ModelForm):
     password = forms.CharField(widget = forms.PasswordInput())
@@ -13,3 +15,27 @@ class UserProfileForm(forms.ModelForm):
     class Meta:
         model = UserProfile
         fields = ("age", "bio", "location", "profilePicture")
+
+
+class PostForm(forms.ModelForm):
+    postTitle = forms.CharField(max_length = 128, help_text = "Enter post title")
+    postImage = forms.ImageField()
+    postDescription = forms.CharField(max_length = 128, help_text = "Enter post description")
+    likes = forms.IntegerField(widget = forms.HiddenInput(), initial = 0)
+    dislikes = forms.IntegerField(widget = forms.HiddenInput(), initial = 0)
+
+    class Meta:
+        model = Post
+        fields = ("postTitle", "postImage", "postDescription")
+
+
+class ListingForm(forms.ModelForm):
+    petName = forms.CharField(max_length = 128, help_text = "Enter the name of the pet for sale")
+    breed = forms.ChoiceField(queryset = PetPedia.breed)
+    petAge = forms.IntegerField(help_text = "How old is your pet?")
+    cost = forms.IntegerField(help_text = "How much does the pet cost?")
+    petImage = forms.ImageField()
+
+    class Meta:
+        model = Listing
+        fields = ("breed", "petName", "petAge", "cost", "petImage")
--- pawbook_wad/models.py
+++ pawbook_wad/models.py
@@ -1,43 +1,47 @@
 from django.db import models
 from django.contrib.auth.models import User
+from datetime import datetime
 
 
-class UserProfile(models.Model):   #User model
-    #Link UserProfile to User model instance
-    user = models.OneToOneField(User, on_delete = models.CASCADE)
+class UserProfile(models.Model):   # User model
+    user = models.OneToOneField(User, on_delete = models.CASCADE)   # Link UserProfile to User model instance
 
+    dateJoined = models.DateField(default = datetime.now())
     age = models.IntegerField()
     bio = models.CharField()
-    sellCount = models.IntegerField(default = 0)
     location = models.CharField()
-    profilePicture = ImageField(upload_to = "profile_image", blank = true)
+    sellCount = models.IntegerField(default=0)
+    profilePicture = models.ImageField(upload_to = "profile_image", blank = True)
 
     def __str__(self):
         return self.user.username
 
 
-class Post(model.Models):           #Image posts model
-    poster = models.ForeignKey(UserProfile, on_delete = models.CASCADE) #Foreign key for user profile
+class Post(models.Model):           # Image posts model
+    poster = models.ForeignKey(UserProfile, on_delete = models.CASCADE)     # Foreign key for user profile
     postTitle = models.CharField(max_length = 128)
-    #datePosted
+    datePosted = models.DateField(default = datetime.now())
     postImage = models.ImageField(upload_to = "post_image")
     postDescription = models.CharField(max_length = 300)
-    Likes = models.IntegerField(default = 0)
-    Dislikes = models.IntegerField(default = 0)
+    likes = models.IntegerField(default = 0)
+    dislikes = models.IntegerField(default = 0)
 
 
-class Listing(model.Models):        #Listing model
+class PetPedia(models.Model):       # Pet-O-Pedia model
+    breed = models.CharField(max_length = 128)
+    species = models.CharField(max_length = 128)
+    info = models.CharField(max_length = 128)
+    picture = models.ImageField(upload_to = "petPedia_image")
+
+
+class Listing(models.Model):        # Listing model
     poster = models.ForeignKey(UserProfile, on_delete = models.CASCADE)
     breed = models.ForeignKey(PetPedia, on_delete = models.CASCADE)
-    #datePosted
+    datePosted = models.DateField(default = datetime.now())
     petName = models.CharField(max_length = 128)
     petAge = models.IntegerField()
     cost = models.IntegerField()
     petImage = models.ImageField(upload_to = "listing_image")
 
 
-class PetPedia(model.Models):       #Pet-O-Pedia model
-    breed = models.CharField(max_length = 128)
-    species = models.CharField(max_length = 128)
-    info = models.CharField(max_length = 128)
-    picture = models.ImageField(upload_to = "petPedia_image")
+
