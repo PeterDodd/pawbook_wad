dd53abf1a0dccef58c59becb0bc6dc4360ae3ac2
Jake Haakanson <2407682H@student.gla.ac.uk>
2020-03-07 18:15:50 +0000

    Added Post, Listing, and PetPedia Models

--- pawbook_wad/models.py
+++ pawbook_wad/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class UserProfile(models.Model):   #User model
     #Link UserProfile to User model instance
     user = models.OneToOneField(User, on_delete = models.CASCADE)
@@ -13,3 +14,30 @@ class UserProfile(models.Model):   #User model
 
     def __str__(self):
         return self.user.username
+
+
+class Post(model.Models):           #Image posts model
+    poster = models.ForeignKey(UserProfile, on_delete = models.CASCADE) #Foreign key for user profile
+    postTitle = models.CharField(max_length = 128)
+    #datePosted
+    postImage = models.ImageField(upload_to = "post_image")
+    postDescription = models.CharField(max_length = 300)
+    Likes = models.IntegerField(default = 0)
+    Dislikes = models.IntegerField(default = 0)
+
+
+class Listing(model.Models):        #Listing model
+    poster = models.ForeignKey(UserProfile, on_delete = models.CASCADE)
+    breed = models.ForeignKey(PetPedia, on_delete = models.CASCADE)
+    #datePosted
+    petName = models.CharField(max_length = 128)
+    petAge = models.IntegerField()
+    cost = models.IntegerField()
+    petImage = models.ImageField(upload_to = "listing_image")
+
+
+class PetPedia(model.Models):       #Pet-O-Pedia model
+    breed = models.CharField(max_length = 128)
+    species = models.CharField(max_length = 128)
+    info = models.CharField(max_length = 128)
+    picture = models.ImageField(upload_to = "petPedia_image")
