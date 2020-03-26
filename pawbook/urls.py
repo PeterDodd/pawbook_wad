2dbf3c95673ab85e7f46c91b6bf2f7d7173e58ce
Jake Haakanson <2407682H@student.gla.ac.uk>
2020-03-26 19:18:25 +0000

    Slight changes, unable to get images working at the moment. Updated gitignore, population script to add images soon.

--- pawbook/urls.py
+++ pawbook/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
+
 from pawbook import views
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 app_name = "pawbook"
 
@@ -22,3 +28,7 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', views.login, name='login'),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+
