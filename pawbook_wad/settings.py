dd6754a263e9aa8a240cde499e0cf2522a56274b
Jake Haakanson <2407682H@student.gla.ac.uk>
2020-03-30 14:34:21 +0100

    Added post boxes to marketplace, pet-o-pedia. New base file baseNoPetorMarket does not include pet info or market search

--- pawbook_wad/settings.py
+++ pawbook_wad/settings.py
@@ -16,6 +16,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
+MEDIA_DIR = os.path.join(BASE_DIR, "media")
 
 
 # Quick-start development settings - unsuitable for production
@@ -123,7 +124,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
 
