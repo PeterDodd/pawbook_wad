b782dc1df45eeedbc1dcde64e59ad405c6ad44cd
Peter Dodd <2308057d@student.gla.ac.uk>
2020-04-04 23:09:28 +0100

    Updated styling

--- pawbook/forms.py
+++ pawbook/forms.py
@@ -9,17 +9,27 @@ class UserForm(forms.ModelForm):        # Form for User objects
     class Meta:
         model = User
         fields = ("username", "email", "password")
+        widgets = {
+            "username": forms.TextInput(attrs={'placeholder': 'USERNAME', 'id': 'top'}),
+            "email": forms.TextInput(attrs={'placeholder': 'EMAIL'}),
+            "password": forms.TextInput(attrs={'placeholder': 'PASSWORD'})}
 
 
 class UserProfileForm(forms.ModelForm): # Form for UserProfile objects
     class Meta:
         model = UserProfile
         fields = ("firstName", "lastName", "age", "bio", "location", "profilePicture")
+        widgets = {
+            "firstName": forms.TextInput(attrs={'placeholder': 'FIRST NAME', 'id': 'top'}),
+            "lastName": forms.TextInput(attrs={'placeholder': 'LAST NAME'}),
+            "age": forms.TextInput(attrs={'placeholder': 'AGE'}),
+            "bio": forms.TextInput(attrs={'placeholder': 'BIO'}),
+            "location": forms.TextInput(attrs={'placeholder': 'LOCATION'})}
 
 
 class PostForm(forms.ModelForm):        # Form for new posts
-    postTitle = forms.CharField(max_length = 128, help_text = "TITLE")
-    postDescription = forms.CharField(max_length = 128, help_text = "DESCRIPTION")
+    postTitle = forms.CharField(max_length = 128, widget=forms.TextInput(attrs={'placeholder': 'TITLE', 'id': 'top'}))
+    postDescription = forms.CharField(max_length = 128, widget=forms.TextInput(attrs={'placeholder': 'DESCRIPTION'}))
 
     likes = forms.IntegerField(widget = forms.HiddenInput(), initial = 0)
     dislikes = forms.IntegerField(widget = forms.HiddenInput(), initial = 0)
@@ -33,12 +43,12 @@ class PostForm(forms.ModelForm):        # Form for new posts
 
 
 class ListingForm(forms.ModelForm):     # Form for new listings
-    breed = forms.CharField(max_length = 128, help_text = "BREED")
-    petName = forms.CharField(max_length = 128, help_text = "PETS NAME")
-    description = forms.CharField(max_length = 500, help_text = "DESCRIPTION")
-    petAge = forms.IntegerField(help_text = "PET AGE")
+    breed = forms.CharField(max_length = 128, widget=forms.TextInput(attrs={'placeholder': 'BREED', 'id': 'top'}))
+    petName = forms.CharField(max_length = 128, widget=forms.TextInput(attrs={'placeholder': 'PET NAME'}))
+    description = forms.CharField(max_length = 500, widget=forms.TextInput(attrs={'placeholder': 'DESCRIPTION'}))
+    petAge = forms.IntegerField(widget=forms.TextInput(attrs={'placeholder': 'PET AGE'}))
 
-    cost = forms.IntegerField(help_text = "PET COST")
+    cost = forms.IntegerField(widget=forms.TextInput(attrs={'placeholder': 'PRICE'}))
     petImage = forms.ImageField()
 
     class Meta:
